eae5a4bd1064dd86ed72d45e7fde1fbe4820f442
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-02-09 11:29:30 -0600

    added a new library called multer to handle uploading files

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js"; 
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import multer from 'multer';
 const app = express();
 
 app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
@@ -14,6 +14,11 @@ app.use(cookieParser());
 // app.get("/test", (req, res) => {
 //     res.json("My ass");
 // }); //test
+const upload = multer({dest: 'uploads/'});
+app.post('/upload', upload.single('file'), function (req, res){
+    res.status(200).json("Image has been uploaded.");
+
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
